476c33c0f47f95833f47591ddad680508b739282
Gagan Chaudhary <gagan.chaudhary@kickdrumtech.com>
2025-03-14 00:02:28 +0530

    pushing for dev env

--- terraform/modules/cloudwatch/variables.tf
+++ terraform/modules/cloudwatch/variables.tf
@@ -8,6 +8,11 @@ variable "environment" {
   type        = string
 }
 
+variable "name_prefix" {
+  description = "Prefix to use for resource names"
+  type        = string
+}
+
 variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
