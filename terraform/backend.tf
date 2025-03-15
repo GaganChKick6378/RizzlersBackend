af24c07d1f0c536798505f00cde75b8c88508ca4
Gagan Chaudhary <gagan.chaudhary@kickdrumtech.com>
2025-03-15 16:35:52 +0530

    stash

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "rizzlers-ibe-dev-tfstate"
-    key    = "env/qa/backend/terraform.tfstate"
-    region = "ap-south-1"
-  }
-} 
--- terraform/modules/security/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_sg_id" {
-  description = "ID of the security group for the ALB"
-  value       = aws_security_group.alb_sg.id
-}
-
-output "ecs_sg_id" {
-  description = "ID of the security group for ECS tasks"
-  value       = aws_security_group.ecs_sg.id
-} 
