aeced3da4ff31c55a1cb911d717d54bdd77335d9
Gagan Chaudhary <gagan.chaudhary@kickdrumtech.com>
2025-03-20 23:57:56 +0530

    addig the configuration controllers

--- backend/src/main/java/com/kdu/rizzlers/controller/TenantConfigurationController.java
+++ backend/src/main/java/com/kdu/rizzlers/controller/TenantConfigurationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tenant-configurations")
+@RequestMapping("/tenant-configurations")
 public class TenantConfigurationController {
 
     @Autowired
